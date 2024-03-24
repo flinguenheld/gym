f586f23d91e3de4970ddc36aee9a41c980afcef7
flo <f@linguenheld.fr>
2024-03-24 17:09:01 +0100

    Add current dir management

--- src/english.rs
+++ src/english.rs
@@ -1,5 +1,6 @@
 use crate::window;
 use rand::Rng;
+use std::env;
 use std::fs;
 use std::io::{stdin, stdout, Stdout, Write};
 use termion::event::Key;
@@ -17,106 +18,119 @@ struct Word {
 
 pub fn run() {
     // Read file and create the word list
-    if let Ok(file_content) = fs::read_to_string("vocabulary.txt") {
-        let mut words: Vec<Word> = Vec::new();
-        file_content.split('\n').for_each(|line| {
-            line.split(';').for_each(|field| {
-                if let Some((a, b)) = field.split_once('=') {
-                    words.push(Word {
-                        value: a.to_string(),
-                        synonyms: b.split(',').map(|val| val.to_string()).collect(),
-                    });
-                }
-            })
-        });
-
-        // Raw mode mandatory to read key events --
-        let stdin = stdin();
-        let mut stdout = stdout().into_raw_mode().unwrap();
-
-        // --
-        let mut success: u16 = 0;
-        let mut fails: u16 = 0;
-        let mut warning = false;
-
-        let mut user_input = String::from("");
-        let mut help = String::from("");
-
-        // Get, check & display the first word --
-        if let Some(mut current_word) = get_random_word(&words) {
-            update_screen(
-                warning,
-                &current_word,
-                &user_input,
-                success,
-                fails,
-                &help,
-                &mut stdout,
-            );
-
-            for c in stdin.keys() {
-                match c.unwrap() {
-                    Key::Esc | Key::Ctrl('c') => break,
-
-                    // Tab to display help (all synonyms with stars).
-                    Key::Char('\t') => {
-                        help = current_word
-                            .synonyms
-                            .join(" - ")
-                            .chars()
-                            .map(|c| {
-                                if c != ' ' && c != '-' && rand::thread_rng().gen_bool(1.0 / 2.0) {
-                                    '*'
-                                } else {
-                                    c
-                                }
-                            })
-                            .collect();
-
-                        user_input.clear();
+    if let Ok(path) = env::current_dir() {
+        println!("{}", path.display());
+        if let Ok(file_content) = fs::read_to_string(format!("{}/vocabulary.txt", path.display())) {
+            let mut words: Vec<Word> = Vec::new();
+            file_content.split('\n').for_each(|line| {
+                line.split(';').for_each(|field| {
+                    if let Some((a, b)) = field.split_once('=') {
+                        words.push(Word {
+                            value: a.to_string(),
+                            synonyms: b.split(',').map(|val| val.to_string()).collect(),
+                        });
                     }
+                })
+            });
 
-                    Key::Char('\n') => {
-                        if current_word
-                            .synonyms
-                            .contains(&user_input.trim().to_string())
-                        {
-                            success += 1;
-                            warning = false;
-                            current_word = get_random_word(&words).unwrap();
-                        } else {
-                            fails += 1;
-                            warning = true;
-                        }
+            // Raw mode mandatory to read key events --
+            let stdin = stdin();
+            let mut stdout = stdout().into_raw_mode().unwrap();
 
-                        user_input.clear();
-                        help.clear();
-                    }
+            // --
+            let mut success: u16 = 0;
+            let mut fails: u16 = 0;
+            let mut warning = false;
 
-                    Key::Backspace => {
-                        user_input.pop();
-                    }
+            let mut user_input = String::from("");
+            let mut help = String::from("");
 
-                    Key::Char(c) => {
-                        user_input.push(c);
-                    }
-
-                    _ => {}
-                }
-
+            // Get, check & display the first word --
+            if let Some(mut current_word) = get_random_word(&words) {
                 update_screen(
                     warning,
-                    &current_word,
+                    current_word,
                     &user_input,
                     success,
                     fails,
                     &help,
                     &mut stdout,
                 );
+
+                for c in stdin.keys() {
+                    match c.unwrap() {
+                        Key::Esc | Key::Ctrl('c') => break,
+
+                        // Tab to display help (all synonyms with stars).
+                        Key::Char('\t') => {
+                            help = current_word
+                                .synonyms
+                                .join(" - ")
+                                .chars()
+                                .map(|c| {
+                                    if c != ' '
+                                        && c != '-'
+                                        && rand::thread_rng().gen_bool(1.0 / 2.0)
+                                    {
+                                        '*'
+                                    } else {
+                                        c
+                                    }
+                                })
+                                .collect();
+
+                            user_input.clear();
+                        }
+
+                        Key::Char('\n') => {
+                            if current_word
+                                .synonyms
+                                .contains(&user_input.trim().to_string())
+                            {
+                                success += 1;
+                                warning = false;
+                                current_word = get_random_word(&words).unwrap();
+                            } else {
+                                fails += 1;
+                                warning = true;
+                            }
+
+                            user_input.clear();
+                            help.clear();
+                        }
+
+                        Key::Backspace => {
+                            user_input.pop();
+                        }
+
+                        Key::Char(c) => {
+                            user_input.push(c);
+                        }
+
+                        _ => {}
+                    }
+
+                    update_screen(
+                        warning,
+                        current_word,
+                        &user_input,
+                        success,
+                        fails,
+                        &help,
+                        &mut stdout,
+                    );
+                }
+            } else {
+                println!("\r");
+                println!("   The file 'vocabulary.txt' is empty or corrupted, please recreate it with this structure:\r");
+                println!("       word=first_synonym,second_synonym,...\r");
+                println!("       word=first_synonym,second_synonym,...\r");
+                println!("       ...\r");
+                println!("\r");
             }
         } else {
             println!("\r");
-            println!("   The file 'vocabulary.txt' is empty or corrupted, please recreate it with this structure:\r");
+            println!("   Error file, please create a file 'vocabulary.txt' in the same folder with this structure:\r");
             println!("       word=first_synonym,second_synonym,...\r");
             println!("       word=first_synonym,second_synonym,...\r");
             println!("       ...\r");
@@ -124,16 +138,13 @@ pub fn run() {
         }
     } else {
         println!("\r");
-        println!("   Error file, please create a file 'vocabulary.txt' in the same folder with this structure:\r");
-        println!("       word=first_synonym,second_synonym,...\r");
-        println!("       word=first_synonym,second_synonym,...\r");
-        println!("       ...\r");
+        println!("   Gym's folder is unreachable");
         println!("\r");
     }
 }
 
 // --
-fn get_random_word(words: &Vec<Word>) -> Option<&Word> {
+fn get_random_word(words: &[Word]) -> Option<&Word> {
     words.get(rand::thread_rng().gen_range(0, words.len()))
 }
 
